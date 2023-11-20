e61a043e8d50652a027161a8326473d79a8e7d27
KevinSimo2022 <k.takoulo@alustudent.com>
2023-11-20 09:01:32 +0200

    Made the remaining files into react format

--- frontend/eden-pages/src/App.js
+++ frontend/eden-pages/src/App.js
@@ -5,12 +5,17 @@ import Navibar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/Footer";
 import Banner from "./components/Banner.js"
-
+import About from "./components/About";
+import Services from "./components/Services"
+import Blogs from "./components/Blogs"
 function App() {
   return (
   <div>
    <Navibar/>
    <Banner/>
+   <About/>
+   <Services/>
+   <Blogs/>
    <Footer/>
   </div>
   );
