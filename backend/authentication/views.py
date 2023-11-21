ff51cfce6cbc003831b5da52dbafe12a35d7c832
uwasejeannine <jeannineuwasee@gmail.com>
2023-11-21 09:32:34 +0200

    add login functionalities

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import authenticate, login
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status, permissions
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
 @api_view(['POST'])
 @permission_classes([permissions.AllowAny])
@@ -16,6 +16,7 @@ def registration_view(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# login view
 @api_view(['POST'])
 @permission_classes([permissions.AllowAny])
 def login_view(request):
@@ -26,9 +27,14 @@ def login_view(request):
 
     if user is not None:
         login(request, user)
+        print("Login successful")
         return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
     else:
+        print("Invalid credentials")
         return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    # get all registered users
+    
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def get_registered_users(request):
