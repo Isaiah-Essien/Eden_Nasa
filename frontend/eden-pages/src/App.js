379171d574a94efe8b5bd7480aaed81520328bb2
Jeannine Uwase <79199207+uwasejeannine@users.noreply.github.com>
2023-12-10 23:24:59 +0200

    Update App.js

--- frontend/eden-pages/src/App.js
+++ frontend/eden-pages/src/App.js
@@ -24,6 +24,8 @@ function App() {
         <Route path="/FaqPage" element={<FaqPage />} />
         <Route path="/RegPage" element={<RegPage />} />
         <Route path="/InsideCartPage" element={<InsideCartPage />} />
+        <Route path="/ProductFormPage" element={<ProductFormPage />} />
+
       </Routes>
     </Router>
   );
